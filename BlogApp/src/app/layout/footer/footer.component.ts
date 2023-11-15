c8737cebf58aaeea4e6aa4df316cc2d89cf1f9f8
Ömer GÜNGÖR <omer.gungor@wsrlife.com>
2023-11-15 17:52:09 +0300

    footer added

--- BlogApp/src/app/layout/footer/footer.component.ts
+++ BlogApp/src/app/layout/footer/footer.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
